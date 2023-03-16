7ad0f9477a468e91c0ddc3c7385e92c97e8f0dbf
duvan1026 <duvancastro1026@gmail.com>
2023-03-16 15:29:21 -0500

    arithmetic operators are added and implemented

--- src/com/example/Operadores.java
+++ src/com/example/Operadores.java
@@ -4,7 +4,39 @@ public class Operadores {
 
     public static void main(String[] args){
 
-        // aritimeticos
+        // aritmeticos
+
+        int numero1 = 10;
+        int numero2 = 20;
+
+        int resultadoSuma = numero1 + numero2;
+
+        System.out.println( "resultadoSuma: " + resultadoSuma);
+        System.out.println("resultadoSuma: " + (resultadoSuma + 5.77));
+
+
+        int resultadoresta = numero1 - numero2;
+
+        /*
+         Comparación
+         > Mayor que
+         < menor que
+         >= mayor igual que
+         <= menor igual que
+         == igual que
+         */
+
+        boolean resultado1 = numero1 > numero2;
+        System.out.println("resultado1: " + resultado1);
+
+
+        boolean resultado2 = numero1 < numero2;
+        System.out.println("resultado2: " + resultado2);
+
+
+
+
+
 
     }
 }
