4decafb47e1dd24419e450ec2d4a0cf24454b206
duvan1026 <duvancastro1026@gmail.com>
2023-03-16 16:04:09 -0500

    implement teh two opticion of function

--- src/com/example/Funciones.java
+++ src/com/example/Funciones.java
@@ -4,12 +4,30 @@ public class Funciones {
 
     public static void main(String[] args){
 
-        // Opcion 1:
+        System.out.println("\n/**** Opcion 1: sin parametros y sin tipo de retorno ****/ ");
+        // Opcion 1: sin parametros y sin tipo de retorno
+        showMenu();
+        showMenu();
+
+        System.out.println("\n/**** Opcion 2: Función sin parametros y con tipo de retorno ****/");
+        // Opcion 2: Función sin parametros y con tipo de retorno
+        System.out.println(getMenu());
 
 
-        // Opcion 2:
 
+    }
 
+    /**
+     * void indica que no devuelve nada
+     */
+    static void showMenu(){
+        System.out.println("bienvenidos al E-Commerce de zapatillas");
+        System.out.println("1- ver zapatillas");
+        System.out.println("2- Comprar zapatilla");
+        System.out.println("3- Salir");
+    }
 
+    static String getMenu(){
+        return "bienvenidos al E-Commerce de zapatillas \n 1 - Ver Zapatillas..... ";
     }
 }
