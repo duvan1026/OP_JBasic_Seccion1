5c7630881e31078f01d995cc72703b5493d2dc9a
Duvan Castro <duvancastro1026@gmail.com>
2023-03-17 04:25:18 -0500

    created and implemented function overload

--- src/com/example/Sobrecarga.java
+++ src/com/example/Sobrecarga.java
@@ -1,4 +1,17 @@
 package com.example;
 
 public class Sobrecarga {
+
+    public static void main(String[] args){
+
+
+    }
+
+    static int suma(int numero1, int numero2){
+        return numero2 + numero1;
+    }
+
+    static int suma(int numero1, int numero2, int numero3){
+        return numero1 + numero2;
+    }
 }
