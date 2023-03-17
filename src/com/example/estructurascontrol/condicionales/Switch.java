f34ff0de71b774b9a780b5b0cbe8e48fcd191991
duvan1026 <duvancastro1026@gmail.com>
2023-03-17 08:50:21 -0500

    update files

--- src/com/example/estructurascontrol/condicionales/Switch.java
+++ src/com/example/estructurascontrol/condicionales/Switch.java
@@ -1,4 +1,44 @@
 package com.example.estructurascontrol.condicionales;
 
 public class Switch {
+
+    public static void main(String[] args){
+
+        String dia = "Viernes";
+
+        switch(dia){
+
+            case "Lunes":
+                System.out.println("Hoy es Lunes!! Animo!!");
+                break;
+
+            case "Martes":
+                System.out.println("Hoy es martes");
+                break;
+
+            case "Miercoles":
+                System.out.println("Hoy es Miercoles!! Animo!!");
+                break;
+
+            case "Jueves":
+                System.out.println("Hoy es Jueves");
+                break;
+            case "Viernes":
+                System.out.println("Hoy es Viernes!! Animo!!");
+                break;
+
+            case "Sabado":
+                System.out.println("Hoy es Sabado");
+                break;
+
+            case "Domingo":
+                System.out.println("Hoy es Domingo");
+                break;
+
+            default:
+                System.out.println("No es un dia valido");
+                break;
+        }
+
+    }
 }
