5dbb9bb7432a7b976378410e8411155986ffa8f3
Duvan Castro <duvancastro1026@gmail.com>
2023-03-17 05:34:46 -0500

    update files

--- src/com/example/estructurascontrol/condicionales/ifmain.java
+++ src/com/example/estructurascontrol/condicionales/ifmain.java
@@ -2,4 +2,19 @@ package com.example.estructurascontrol.condicionales;
 
 public class ifmain {
 
+    public static void main(String[] args){
+
+        int edad = 12;
+        boolean esMayor = ( edad >= 18 );
+
+        if( esMayor ){
+            System.out.println("Es Mayor de edad");
+        }else {
+            System.out.println("Es Menor de edad");
+        }
+
+
+
+    }
+
 }
