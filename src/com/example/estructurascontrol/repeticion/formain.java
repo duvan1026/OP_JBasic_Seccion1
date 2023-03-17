3eab1827da8a46d24e0128da44d93d0547d57b73
duvan1026 <duvancastro1026@gmail.com>
2023-03-17 09:29:35 -0500

    implementation for in Java

--- src/com/example/estructurascontrol/repeticion/formain.java
+++ src/com/example/estructurascontrol/repeticion/formain.java
@@ -4,6 +4,24 @@ public class formain {
 
     public static  void main(String[] args){
 
+        for( int i = 0; i < 10; i++ ){
+            System.out.println("El valor de i es: " + i );
+        }
+
+        String[] nombres = { "Pepe", "Juanito", "Roberta" };
+
+        for( int i = 0; i < nombres.length; i++ ){
+            System.out.println(nombres[i]);
+        }
+
+        int[] numeros = {5, 7, 8};
+        int suma = 0;
+        for( int i = 0; i < numeros.length; i++ ){
+            suma += numeros[i];
+            System.out.println(numeros[i]);
+        }
+
+        System.out.println(suma);
 
     }
 }
