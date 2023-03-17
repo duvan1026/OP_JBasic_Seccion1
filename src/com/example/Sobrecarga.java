137e3208ffb855e127858d966431a036c8de93fa
Duvan Castro <duvancastro1026@gmail.com>
2023-03-17 05:16:43 -0500

    update file

--- src/com/example/Sobrecarga.java
+++ src/com/example/Sobrecarga.java
@@ -1,10 +1,17 @@
 package com.example;
 
+/**
+ * Sobrecarga: me permite duplicar un método siempre y cuando
+ * tenga diferentes numero/tipo parametros
+ */
 public class Sobrecarga {
 
     public static void main(String[] args){
 
+    }
 
+    static double suma(double numero1, int numero2){
+        return numero2 + numero1;
     }
 
     static int suma(int numero1, int numero2){
