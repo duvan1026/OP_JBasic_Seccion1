3d371ae3c131633265573bdd05b52e14b2cad1e2
duvan1026 <duvancastro1026@gmail.com>
2023-03-17 10:06:33 -0500

    implemented while in Java

--- src/com/example/estructurascontrol/repeticion/whilemain.java
+++ src/com/example/estructurascontrol/repeticion/whilemain.java
@@ -4,6 +4,18 @@ public class whilemain {
 
     public static  void main(String[] args){
 
+        int contador = 0;
+
+
+        while(contador  < 10){
+            contador++;
+            if( contador == 5 ){
+//                break;
+                continue;
+            }
+            System.out.println("Valor de contador : " + contador);
+        }
+
     }
 
 }
